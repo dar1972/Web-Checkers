0bdfbb85f536da7e39791f0bf289852644e9417b
Marcus Kapoor <mok8867@rit.edu>
2020-10-20 16:51:29 -0400

    removed warnings

--- src/main/java/com/webcheckers/ui/SprintTwoRoutes/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/SprintTwoRoutes/PostCheckTurnRoute.java
@@ -1,25 +1,18 @@
 package com.webcheckers.ui.SprintTwoRoutes;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.webcheckers.appl.GameCenter;
-import com.webcheckers.model.Game;
-import com.webcheckers.model.Player;
 import com.webcheckers.ui.PostSignInRoute;
 import com.webcheckers.util.Message;
 import spark.*;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
-import java.util.logging.ErrorManager;
 import java.util.logging.Logger;
 
 //Made and worked on my Beck Anderson
 
 public class PostCheckTurnRoute implements Route{
 
-    private static final String OPPONENT_RESIGN = "Your opponent resigned, so you win! Please head back to the home menu.";
     public static final String GAME_WON = "You won the game :D Please head back to the home menu.";
     public static final String GAME_LOST = "You lost the game :( Please head back to the home menu.";
 
@@ -27,9 +20,8 @@ public class PostCheckTurnRoute implements Route{
 
     private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
 
-    private Game gameID;
     private final Gson gson;
-    private final GameCenter gameCenter;
+    //private final GameCenter gameCenter;
 
     /**
      * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP
@@ -38,7 +30,7 @@ public class PostCheckTurnRoute implements Route{
      */
     public PostCheckTurnRoute(GameCenter gameCenter,Gson gson) {
         Objects.requireNonNull( gameCenter );
-        this.gameCenter = gameCenter;
+        //this.gameCenter = gameCenter;
         this.gson = gson;
         LOG.config("PostCheckTurnRoute is initialized.");
     }
@@ -48,19 +40,14 @@ public class PostCheckTurnRoute implements Route{
         LOG.finer("PostCheckTurnRoute is invoked.");
 
         String name = request.session().attribute(USER_PARAM);
-        String outcome = Boolean.toString(gameCenter.isPlayerActive(name));
+        //String outcome = Boolean.toString(gameCenter.isPlayerActive(name));
         String winner = "gameCenter.gameWon(name);";
         Message message;
 
-        if(winner!=null){
-            if(winner.equals(name)){
-                message = Message.info(GAME_WON);
-            }else{
-                message = Message.info(GAME_LOST);
-            }
-        }
-        else{
-            message = Message.info(outcome);
+        if (winner.equals(name)) {
+            message = Message.info(GAME_WON);
+        } else {
+            message = Message.info(GAME_LOST);
         }
 
         String json;
